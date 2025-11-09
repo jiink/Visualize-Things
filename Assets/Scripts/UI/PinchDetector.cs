0f92acf2ca0813b7d4568fcf201b3ed9f563d3bf
Jacob C <jacob.j.carlson03@gmail.com>
2025-11-08 19:39:35 -0800

    pinch to bring up a prefabb

---
+++ Assets/Scripts/UI/PinchDetector.cs
@@ -0,0 +1,51 @@
+using System;
+using UnityEngine;
+
+public delegate void HandPinchEventHandler(object sender, OVRPlugin.Hand hand, OVRHand.HandFinger finger, bool state);
+
+public class PinchDetector : MonoBehaviour
+{
+    [SerializeField]
+    private OVRHand _ovrHandLeft;
+    [SerializeField]
+    private OVRHand _ovrHandRight;
+
+    private bool _wasLeftRingPinching = false;
+    private bool _wasLeftMiddlePinching = false;
+    private bool _wasRightRingPinching = false;
+    private bool _wasRightMiddlePinching = false;
+
+    public event HandPinchEventHandler PinchEvent;
+
+    void Start()
+    {
+    }
+
+    private void CheckPinchState(OVRHand hand, OVRHand.HandFinger finger, ref bool wasPinching)
+    {
+        if (!hand.IsDataValid || !hand.IsDataHighConfidence)
+        {
+            return;
+        }
+        //bool isPinching = hand.GetFingerIsPinching(finger);
+        bool isPinching = hand.GetFingerPinchStrength(finger) > 0.77f;
+        if (isPinching && !wasPinching)
+        {
+            PinchEvent?.Invoke(this, hand.GetHand(), finger, true);
+        }
+        else if (!isPinching && wasPinching)
+        {
+            PinchEvent?.Invoke(this, hand.GetHand(), finger, false);
+        }
+        wasPinching = isPinching;
+    }
+
+    void Update()
+    {
+        Debug.Log($"GetFingerPinchStrength: {_ovrHandLeft.GetFingerPinchStrength(OVRHand.HandFinger.Middle):F2}");
+        CheckPinchState(_ovrHandLeft, OVRHand.HandFinger.Ring, ref _wasLeftRingPinching);
+        CheckPinchState(_ovrHandLeft, OVRHand.HandFinger.Middle, ref _wasLeftMiddlePinching);
+        CheckPinchState(_ovrHandRight, OVRHand.HandFinger.Ring, ref _wasRightRingPinching);
+        CheckPinchState(_ovrHandRight, OVRHand.HandFinger.Middle, ref _wasRightMiddlePinching);
+    }
+}
---
+++ Assets/Scripts/UI/RadialMenuController.cs
@@ -0,0 +1,89 @@
+using Oculus.Interaction;
+using Oculus.Interaction.Input;
+using System;
+using UnityEngine;
+
+public class RadialMenuController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _radialMenuPrefab;
+    [SerializeField]
+    private Transform _headPos;
+    [SerializeField]
+    private OVRHand _ovrHandLeft;
+    [SerializeField]
+    private OVRHand _ovrHandRight;
+    [SerializeField]
+    private PinchDetector _pinchDetector;
+
+    private GameObject _radialMenu;
+    private OVRPlugin.Hand _activeHand;
+
+    public enum RadialMenuKind
+    {
+        Primary,
+        Secondary
+    };
+    
+    
+
+    void Start()
+    {
+        _pinchDetector.PinchEvent += OnPinch;
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnPinch(object sender, OVRPlugin.Hand hand, OVRHand.HandFinger finger, bool state)
+    {
+        if (state)
+        {
+            Debug.Log($"PINCH BEGIN!! {hand} {finger}");
+            RadialMenuKind kind = finger switch
+            {
+                OVRHand.HandFinger.Middle => RadialMenuKind.Primary,
+                OVRHand.HandFinger.Ring => RadialMenuKind.Secondary,
+                _ => throw new Exception($"Invalid finger {finger}")
+            };
+            Transform handT = hand switch
+            {
+                OVRPlugin.Hand.HandLeft => _ovrHandLeft.PointerPose,
+                OVRPlugin.Hand.HandRight => _ovrHandRight.PointerPose,
+                _ => throw new Exception($"Invalid hand {hand}")
+            };
+            _activeHand = hand;
+            ShowRadialMenu(kind, handT);
+        } else
+        {
+            if (hand != _activeHand)
+            {
+                return;
+            }
+            Debug.Log($"PINCH END!! {hand} {finger}");
+            HideRadialMenu();
+        }
+    }
+
+    private void ShowRadialMenu(RadialMenuKind kind, Transform hand)
+    {
+        if (_radialMenu != null)
+        {
+            return;
+        }
+        Debug.Log("HERE!");
+        _radialMenu = Instantiate(_radialMenuPrefab);
+        _radialMenu.transform.position = hand.position;
+        _radialMenu.transform.LookAt(_headPos);
+    }
+
+    private void HideRadialMenu()
+    {
+        if (_radialMenu != null)
+        {
+            GameObject.Destroy(_radialMenu);
+        }
+    }
+}
