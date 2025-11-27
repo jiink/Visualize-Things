458cf2d5d76c12ac5f572fd33a0f3e2ceca79167
Jacob C <jacob.j.carlson03@gmail.com>
2025-11-27 12:06:12 -0800

    change room material when toggling occlusion

--- Assets/Scripts/OcclusionService.cs
+++ Assets/Scripts/OcclusionService.cs
@@ -1,11 +1,17 @@
+using Meta.XR.EnvironmentDepth;
+using Meta.XR.MRUtilityKit;
 using UnityEngine;
 
 public class OcclusionService : MonoBehaviour
 {
+    [SerializeField] private EnvironmentDepthManager _environmentDepthManager;
     [SerializeField] private SkinnedMeshRenderer _leftHandRenderer; // see OVRInteractionComprehensive>OVRLeftHandVisual>OpenXRLeftHand>LeftHand
     [SerializeField] private SkinnedMeshRenderer _rightHandRenderer;
     [SerializeField] private Material _handPassthroughMaterial; // For when occlusion is enabled, hand is masked out
     [SerializeField] private Material _handStandardMaterial; // For when occlusion is off
+    [SerializeField] private EffectMesh _effectMesh;
+    [SerializeField] private Material _roomPassthroughMaterial;
+    [SerializeField] private Material _roomVisbileMaterial;
     private bool _occlusionEnabled = true;
     public void ToggleOcclusion()
     {
@@ -19,6 +25,10 @@ public class OcclusionService : MonoBehaviour
             _handPassthroughMaterial : _handStandardMaterial;
         _leftHandRenderer.sharedMaterial = handMat;
         _rightHandRenderer.sharedMaterial = handMat;
+        _environmentDepthManager.enabled = _occlusionEnabled;
+        _environmentDepthManager.OcclusionShadersMode = _occlusionEnabled ?
+            OcclusionShadersMode.SoftOcclusion : OcclusionShadersMode.None;
+        _effectMesh.OverrideEffectMaterial(_occlusionEnabled ? _roomPassthroughMaterial : _roomVisbileMaterial);
     }
     private void Start()
     {
