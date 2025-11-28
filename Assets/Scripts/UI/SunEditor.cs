747db2332deec0bbb8b28c9acfcbb018f0fb363d
Jacob C <jacob.j.carlson03@gmail.com>
2025-11-27 22:30:37 -0800

    sun editor color and shadows work

--- Assets/Scripts/UI/SunEditor.cs
+++ Assets/Scripts/UI/SunEditor.cs
@@ -13,7 +13,24 @@ public class SunEditor : MonoBehaviour
     private void Update()
     {
         if (_dirLight == null || _sunThing == null) { return; }
-        _dirLight.intensity = _sunThing.Magnitude * 5.0f;
+        _dirLight.intensity = _sunThing.Magnitude * 8.0f;
         _dirLight.transform.rotation = _sunThing.Direction;
+    }
+
+    public void SetLightHue(float h)
+    {
+        Color.RGBToHSV(_dirLight.color, out var _, out var s, out var v);
+        _dirLight.color = Color.HSVToRGB(h, s, v);
+    }
+
+    public void SetLightSaturation(float s)
+    {
+        Color.RGBToHSV(_dirLight.color, out var h, out var _, out var v);
+        _dirLight.color = Color.HSVToRGB(h, s, v);
+    }
+
+    public void SetShadows(bool s)
+    {
+        _dirLight.shadows = s ? LightShadows.Soft : LightShadows.None;
     }
 }
