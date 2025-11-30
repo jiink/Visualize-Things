d97d3822dbbc7f17bdbc19a16597b948ea309cb6
Jacob C <jacob.j.carlson03@gmail.com>
2025-11-29 22:24:33 -0800

    material button hovering stuff

---
+++ Assets/Scripts/UI/MaterialButton.cs
@@ -0,0 +1,37 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MaterialButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Material _material;
+    [SerializeField] private TextMeshProUGUI _label;
+    public Material Material => _material;
+    public event EventHandler HoverEvent;
+    public event EventHandler UnhoverEvent;
+    public event EventHandler PressEvent;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log(">>>> HOVER");
+        HoverEvent?.Invoke(this, EventArgs.Empty);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log(">>>> UNHOVER");
+        UnhoverEvent?.Invoke(this, EventArgs.Empty);
+    }
+
+    public void OnPress()
+    {
+        PressEvent?.Invoke(this, EventArgs.Empty);
+    }
+
+    public void Setup(int number, Material material)
+    {
+        _label.text = $"#{number}";
+        _material = material;
+    }
+}
--- Assets/Scripts/UI/MaterialEditorMenu.cs
+++ Assets/Scripts/UI/MaterialEditorMenu.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class MaterialEditorMenu : MonoBehaviour
 {
+    private Dictionary<Material, Color> _originalColors = new();
     private GameObject _inspectedOb;
     public GameObject InspectedObject
     {
@@ -17,38 +18,107 @@ public class MaterialEditorMenu : MonoBehaviour
             _inspectedOb = value;
             if (_inspectedOb != null)
             {
-                gameObjectNameLabel.text = _inspectedOb.name;
+                _gameObjectNameLabel.text = _inspectedOb.name;
                 AddMaterialEntries(_inspectedOb);
             }
         }
     }
-    public TextMeshProUGUI gameObjectNameLabel;
-    public GameObject materialListingPrefab;
-    public Transform materialListingsParent;
+    public TextMeshProUGUI _gameObjectNameLabel;
+    public GameObject _materialButtonPrefab;
+    public Transform _materialButtonsParent;
 
     // Add all materials from the object to the menu
     void AddMaterialEntries(GameObject model)
     {
         // Clean existing entries
-        foreach (Transform child in materialListingsParent)
+        foreach (Transform child in _materialButtonsParent)
         {
             Destroy(child.gameObject);
         }
+        _originalColors.Clear();
+        int materialCounter = 0;
         Renderer[] renderers = model.GetComponentsInChildren<Renderer>();
         if (renderers.Count() > 0)
         {
             foreach (Renderer renderer in renderers)
             {
-                Material[] materialsArray = renderer.materials;
+                Material[] materialsArray = renderer.materials; // do .sharedMaterials instead if you dont want to make copies...
                 List<Material> materials = new(materialsArray);
                 // Process the materials list as needed
                 for (int i = 0; i < materialsArray.Length; i++)
                 {
-                    MaterialListing materialListing = Instantiate(materialListingPrefab, materialListingsParent).GetComponent<MaterialListing>();
-                    materialListing.MaterialNum = i;
-                    materialListing.Material = materialsArray[i];
+                    Material currentMat = materialsArray[i];
+                    if (!_originalColors.ContainsKey(currentMat))
+                    {
+                        if (currentMat.HasProperty("_Color"))
+                        {
+                            _originalColors.Add(currentMat, currentMat.color);
+                        }
+                        else
+                        {
+                            Debug.LogWarning("this doesnt have a color property...");
+                        }
+                    }
+                    MaterialButton matBtn = Instantiate(_materialButtonPrefab, _materialButtonsParent).GetComponent<MaterialButton>();
+                    matBtn.Setup(materialCounter, currentMat);
+                    materialCounter++;
+                    matBtn.HoverEvent += (o, _) => {
+                        MaterialButton mb = o as MaterialButton;
+                        if (mb != null)
+                        {
+                            OnMaterialBtnHover(mb.Material);
+                        }
+                        else
+                        {
+                            Debug.LogError("Couldn't convert sender into MaterialButton");
+                        }
+                    };
+                    matBtn.UnhoverEvent += (o, _) => {
+                        MaterialButton mb = o as MaterialButton;
+                        if (mb != null)
+                        {
+                            OnMaterialBtnUnhover(mb.Material);
+                        }
+                        else
+                        {
+                            Debug.LogError("Couldn't convert sender into MaterialButton");
+                        }
+                    };
+                    matBtn.PressEvent += (_, _) => { Debug.Log(">>>> matBtn.PressEvent happened"); };
                 }
             }
         }
+    }
+
+    void OnMaterialBtnHover(Material mat)
+    {
+        if (mat == null)
+        {
+            Debug.LogError("Null material");
+            return;
+        }
+        if (!_originalColors.ContainsKey(mat) && mat.HasProperty("_Color"))
+        {
+            _originalColors[mat] = mat.color;
+        }
+        mat.color = Color.magenta;
+    }
+
+    void OnMaterialBtnUnhover(Material mat)
+    {
+        if (mat == null)
+        {
+            Debug.LogError("Null material");
+            return;
+        }
+        if (_originalColors.TryGetValue(mat, out Color originalColor))
+        {
+            mat.color = originalColor;
+        }
+        else
+        {
+            Debug.LogWarning("didnt find original color for material: " + mat.name);
+        }
+
     }
 }
